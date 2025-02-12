fdee2cc5b7cce6baba3fcf1a51d0292cf2e6ac19
hilatsivion <hilatsivion2@gmail.com>
2025-02-12 22:51:53 +0200

    update for test

--- falcon-frontend/src/App.js
+++ falcon-frontend/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hila</div>;
+  return <div className="App">hila tsivion</div>;
 }
 
 export default App;
