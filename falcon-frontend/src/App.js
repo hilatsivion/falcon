48ba58c66a3ab6d6b9ad43f0709151e8da9c4688
hilatsivion <hilatsivion2@gmail.com>
2025-02-14 22:50:42 +0200

    update routes

--- falcon-frontend/src/App.js
+++ falcon-frontend/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes";
 import "./styles/global.css";
 
 const App = () => {
   return (
-    <BrowserRouter
-      basename={process.env.NODE_ENV === "production" ? "/falcon" : "/"}
-    >
+    <Router basename={process.env.NODE_ENV === "production" ? "/falcon" : "/"}>
       <AppRoutes />
-    </BrowserRouter>
+    </Router>
   );
 };
 
